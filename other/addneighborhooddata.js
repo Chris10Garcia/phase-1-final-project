fcd04ec7d36c1d32d47c878071663c2de30e1f91
Christian Garcia <bones192nd@gmail.com>
2023-03-01 15:15:24 -0500

    download nyc neighborhood CSV and wrote pseudocode to handle it

---
+++ other/addneighborhooddata.js
@@ -0,0 +1,16 @@
+
+
+// create function to read nystate json file
+
+
+// create function to read nyc csv file
+
+
+// create function to use nyc csv data and build dictionary with keys as zip code and values as [bourough, neighborhood]
+
+
+// create function that reads each brewery and compares it to nyc dictionary
+    // if it's there, add neighborhood / bourough, push brewery into array
+
+
+// save new array into json file
